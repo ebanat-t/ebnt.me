39ab568239a2b01a8a9237106655c8db0e92454d
Ebnt <ebnt.to@gmail.com>
2023-01-21 07:46:46 +0900

    fixed style

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import top from "./components/top.vue"
 </script>
 
 <template>
-  <img src="./assets/hitsuji.jpg" alt="An image of hitsuji-san" class="profile-img">
+  <div class="profile-img-shadow">
+    <img src="./assets/hitsuji.jpg" alt="An image of hitsuji-san" class="profile-img">
+  </div>
   <h1> Ebnt </h1>
   <top></top>
 </template>
--- src/components/top.vue
+++ src/components/top.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <p>ねこかわいいね</p>
+    <div class="profile-card">
+        <p class="message">ねこかわいいね</p>
         <div class="link-v-container">
             <a href="https://twitter.com/ebanatt">TW @ebanatt</a>
             <a href="https://instagram.com/ebanat.t">IG @ebanat.t</a>
