1d038587c76eba8a65f6e59aa584cac8267d4d83
Ebnt <ebnt.to@gmail.com>
2023-01-21 04:15:33 +0900

    prepared temporary profile

---
+++ src/components/top.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>ねこかわいいね</p>
+        <div class="link-v-container">
+            <a href="https://twitter.com/ebanatt">TW @ebanatt</a>
+            <a href="https://instagram.com/ebanat.t">IG @ebanat.t</a>
+            <a href="https://instagram.com/ebntf">IG @ebntf</a>
+        </div>
+       
+    </div>
+</template>
