c229371811ac133d0e15d8412fa58ec1d287977a
ebanat-t <61115724+ebanat-t@users.noreply.github.com>
2023-07-02 15:11:27 +0900

    Update top.vue
    Updated list of Social Profiles

--- src/components/top.vue
+++ src/components/top.vue
@@ -5,6 +5,8 @@
             <a href="https://twitter.com/ebanatt">TW @ebanatt</a>
             <a href="https://instagram.com/ebanat.t">IG @ebanat.t</a>
             <a href="https://instagram.com/ebntf">IG @ebntf</a>
+            <a href="https://bsky.app/profile/ebnt.me">Bluesky @ebnt.me</a>
+            <a href="https://discord.com/users/603984163070148608">Discord ebntt</a>
         </div>
        
     </div>
